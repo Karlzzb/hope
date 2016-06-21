f97169ef8b5d3670fb4876920978490c2081d7f4
Karlzzb <lansedepiaomiao@gmail.com>
2016-06-21 10:59:00 +0800

    Create Topbar.jsx

---
+++ src/components/Topbar.jsx
@@ -0,0 +1,35 @@
+import React, { Component, PropTypes } from 'react'
+
+export default class TopBar extends Component {
+  constructor(props) {
+    super(props);
+    this.onLoadBest = this.onLoadRecommend.bind(this);
+    this.onLoadRecommend = this.onLoadRecommend.bind(this);
+    this.onLoadTypes = this.onLoadRecommend.bind(this);
+  }
+  
+  render() {
+    return(
+    	<div className="topbar">
+    	<button onClick={this.props.onLoadBest} disabled=this.props.isBestLoaded>
+    	  <h1>Best</h1>
+    	</button>
+    	<button onClick={this.props.onLoadRecommend} disabled=this.props.isRecommendLoaded>
+    	  <h1>Recommend</h1>
+    	</button>
+    	<button onClick={this.props.onLoadTypes} disabled=this.props.isTypesLoaded>
+    	  <h1>classfication</h1>
+    	</button>
+    	</div>
+	    )
+  }
+}
+
+TopBar.propTypes = {
+    onLoadBest: PropTypes.func.isRequired,
+    onLoadRecommend: PropTypes.func.isRequired,
+    onLoadTypes: PropTypes.func.isRequired,
+    isBestLoaded: PropTypes.string.isRequired,
+    isRecommendLoaded: PropTypes.string.isRequired,
+    isTypesLoaded: PropTypes.string.isRequired
+}
