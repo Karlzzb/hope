24452b6b2fdef974b4de4d6636aae5456833f3e2
Karlzzb <lansedepiaomiao@gmail.com>
2016-06-21 10:18:35 +0800

    built first two containers: RecommendShow and  BestSellerShow

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Route } from 'react-router'
-import App from './containers/App'
-import Root from './containers/ItemShow'
+import BestSeller from './containers/BestSellerShow'
+import Recommend from './containers/RecommendShow'
 
 export default (
-  <Route path="/" component={Root}>
+  <Route path="/" component={RecommendShow}>
+    <Route path="/recommend" component={Recommend}/>
+    <Route path="/bestSellerShow" component={BestSeller}/>
   </Route>
 )
